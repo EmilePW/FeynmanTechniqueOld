1987035f0d932584aa6f5450858c1654935139df
EmilePW <emilepw@hotmail.co.uk>
2016-06-27 22:57:59 +0100

    Changed reading age calculation to be based on Gunning-Fog (which still sucks)

---
+++ readingAge/readingAge.js
@@ -0,0 +1,20 @@
+var terminalPunctuation = /\.+|\?+|!+/
+
+var topFiveThousandWords = require(__dirname + '/top5000Words')
+
+topFiveThousandWords = topFiveThousandWords.map(function(rank) { return rank["Word"] })
+
+function readingAge (text) {
+  // Calculates accorign tot he Gunning-Fog Readability Score
+
+  if (!text)
+    return false
+
+  var words = text.split(' ').map(function (word) { return word.replace(/[^\w|^'|^-]/g, '') }),
+      sentences = terminalPunctuation.test(text) ? text.split(terminalPunctuation).slice(0, -1) : [text],
+      rarerWords = words.filter(function (word) { return topFiveThousandWords.indexOf(word.toLowerCase()) < 0 })
+
+  return Math.floor(0.4 * ((words.length / sentences.length) + 50 * (rarerWords.length / words.length)))
+}
+
+module.exports = readingAge
--- tests/readingAge.spec.js
+++ tests/readingAge.spec.js
@@ -1,16 +1,16 @@
 import expect from 'expect'
-import calculateReadingAge from '../js/readingAge'
+import readingAge from '../js/readingAge'
 
 describe('readingAge', () => {
 
   it('should return the correct reading age', () => {
-    const actual = calculateReadingAge('The quick brown fox jumps over the lazy, quantum effervescent dog')
-    const expected = 7
+    const actual = readingAge('The quick brown fox jumps over the lazy, quantum effervescent dog')
+    const expected = 13
     expect(actual).toEqual(expected)
   })
 
   it('should return false if there is no text', () => {
-    const actual = calculateReadingAge('')
+    const actual = readingAge('')
     const expected = false
     expect(actual).toEqual(expected)
   })
